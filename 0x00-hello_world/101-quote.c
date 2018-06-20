c7bf9b574b90a9fa9042462b6b52fc8c43bc8345
leobyeon <386@holbertonschool.com>
2018-06-20 23:07:47 +0000

    Fixing 101?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 
+/**
+ * main - writes a sentence to stderr
+ * Return: 1
+*/
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
 	return (1);
 }
