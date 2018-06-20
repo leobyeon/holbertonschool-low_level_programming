0b090606b25e441d18897fe0f77538dbe1c71996
leobyeon <386@holbertonschool.com>
2018-06-20 22:59:04 +0000

    Adding 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <errno.h>
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
