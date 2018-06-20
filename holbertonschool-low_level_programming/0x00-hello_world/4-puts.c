38620d4ff8faa1c76d2e9b30c3266f8b30a41ed3
leobyeon <386@holbertonschool.com>
2018-06-20 19:27:37 +0000

    Adding all the projects for first check

---
+++ holbertonschool-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Puts a sentence
+ * Return: 0 if everything works
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
