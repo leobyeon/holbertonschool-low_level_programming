010427647983d865a19726fe2eac3adeefc5c305
leobyeon <386@holbertonschool.com>
2018-06-21 22:40:59 +0000

    Added 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits separated by a comma and a space,
+ * followed by a new line
+ * Return: 0 if everything works
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+			for (m = '0'; m <= '9'; m++)
+			{
+				putchar(n);
+				putchar(m);
+
+				if (n == '9' && m == '9')
+				{
+				continue;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
