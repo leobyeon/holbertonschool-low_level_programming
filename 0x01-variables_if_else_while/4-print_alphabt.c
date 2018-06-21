53dda9f0a63fbd716cad3ee0b03dab2602116f85
leobyeon <386@holbertonschool.com>
2018-06-21 21:19:56 +0000

    Adding 2 and 4(3?)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase except for e and q,
+ * followed by a new line
+ * Return: 0 if everything works
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' || ch != 'q')
+			putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
