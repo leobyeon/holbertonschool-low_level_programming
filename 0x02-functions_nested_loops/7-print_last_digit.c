7b0fb63c1ce33bc93ad8423942baf8a41edd2014
leobyeon <386@holbertonschool.com>
2018-06-26 17:50:45 +0000

    Fixed 7?

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,13 @@ int print_last_digit(int n)
 	int last;
 
 	if (n < 0)
-	{
 		n = n * -1;
-		last = n % 10;
-		_putchar(last + '0');
-		return (last);
-	}
 	else
-	{
-		last = n % 10;
-		_putchar(last + '0');
-		return (last);
-	}
+		n = n;
+
+	last = n % 10;
+
+	_putchar(last + '0');
+
+	return (last);
 }
