53c6731cf6aca2f3d5c1c0db57fbc89d1cd38a1f
leobyeon <386@holbertonschool.com>
2018-06-27 20:58:02 +0000

    Adding 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the even values
+ * in the fibonacci sequence
+ * as long as the value is less than 4,000,000
+ * Return: 0 if everything works
+ */
+int main(void)
+{
+	int i, sum;
+	long int prev = 1, curr = 2, next;
+
+	sum = 0;
+
+	while (sum < 4000000)
+	{
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+		next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
