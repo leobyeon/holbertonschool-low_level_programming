70c41aa0b98f062d41cad9ac3750f7d5df2fd827
leobyeon <386@holbertonschool.com>
2018-06-27 18:09:57 +0000

    Fixed 102?

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints the first 50 iterations of fibonacci
+ * Return: 0 if everything works
+ */
 int main(void)
 {
 	int i;
@@ -9,11 +13,12 @@ int main(void)
 	curr = 1;
 	printf("%li\n", curr);
 
-	for (i = 2; i <= 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		next = prev + curr;
 		printf("%li\n", next);
 		prev = curr;
 		curr = next;
 	}
+	return (0);
 }
