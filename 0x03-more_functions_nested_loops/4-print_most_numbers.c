198c0725e4c1ad8e3cca5da0ff0a6b12776799d3
leobyeon <386@holbertonschool.com>
2018-06-28 22:33:00 +0000

    Fixed 4; changed the if else around

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,14 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
 			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
