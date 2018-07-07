2e54ba1aa33e42c26c93179fab3a2776773269ba
leobyeon <386@holbertonschool.com>
2018-07-07 22:37:17 +0000

    Trying to fix 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -10,18 +10,12 @@ char *rot13(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str && str[i]; ++i)
 	{
-		if ((*(str + i) >= 'a' && *(str + i) < 'n')
-				|| (*(str + i) >= 'A' && *(str + i) < 'N'))
-		{
-			*(str + i) += 13;
-		}
-		else if ((*(str + i) >= 'n' && *(str + i) <= 'z')
-				|| (*(str + i) >= 'N' && *(str + i) <= 'Z'))
-		{
-			*(str + i) -= 13;
-		}
+		if ((str[i] >= 'a' && (str[i] + 13) <= 'z') || ((str[i] >= 'A' && (str[i] + 13) <= 'Z')))
+				{
+					str[i] += 13;
+				}
 	}
 	return (str);
 }
