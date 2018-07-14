26c86d1a2e11b5bd38a3170f0c179dd402e847ae
leobyeon <386@holbertonschool.com>
2018-07-14 20:14:52 +0000

    Why won't you let me PASS

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
-			if (!isdigit(atoi(argv[i][j])))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
