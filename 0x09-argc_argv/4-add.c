66374e0de1f3af455f88f7e11ccbd07adab07005
leobyeon <386@holbertonschool.com>
2018-07-14 20:32:45 +0000

    4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -16,12 +16,6 @@ int main(int argc, char *argv[])
 	int i, j, sum;
 	sum = 0;
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -36,7 +30,7 @@ int main(int argc, char *argv[])
 		sum += atoi(argv[i]);
 	}
 
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
