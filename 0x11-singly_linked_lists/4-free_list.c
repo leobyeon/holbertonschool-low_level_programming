1ed9d6dcc44a873f909cbe601910731f28e8a10a
leobyeon <386@holbertonschool.com>
2018-08-03 00:24:11 +0000

    Adding 4 and updated the header with the prototype for 4

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * free_list - frees a list_t list
+  * @head: pointer to the first element in the list
+  * Return: void
+  */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (!head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
