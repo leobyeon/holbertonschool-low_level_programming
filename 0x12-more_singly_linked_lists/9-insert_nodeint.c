65bbf822e1f7c8a2ff28071ba2af6ca007b06def
leobyeon <386@holbertonschool.com>
2018-08-07 21:20:19 +0000

    Fixed betty check on 9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = node;
 		return (*head);
 	}
-	for (i = 0; i < idx-1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		prev = prev->next;
 		if (!prev)
