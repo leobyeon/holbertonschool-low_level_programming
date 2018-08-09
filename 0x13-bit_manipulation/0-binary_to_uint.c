2b4cd5d4e0b38e7e2934eeb61ee519819c8b8f46
leobyeon <386@holbertonschool.com>
2018-08-09 16:32:33 +0000

    Fixed the if statement within for loop

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
   * binary_to_uint - converts a binary num to unsigned int
@@ -17,10 +18,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		accum = 2 * accum + (b[i] - '0');
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		accum = 2 * accum + (b[i] - '0');
 	}
-	
 	return (accum);
 }
