a5b5c9521fc4a50d036ad8961271318f19a62da5
leobyeon <386@holbertonschool.com>
2018-08-09 23:56:43 +0000

    Adding 5

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * flip_bits - returns the number of bits you would need
+  * to flip to get from one number to another
+  * @n: number to flip
+  * @m: number to get
+  * Return: number of bits you need to flip
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flips = n ^ m;
+	unsigned int count = 0;
+
+	while (flips)
+	{
+		if (flips & 1)
+			count++;
+		flips >>= 1;
+	}
+	return (count);
+}
