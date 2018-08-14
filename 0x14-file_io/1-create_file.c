e1238cab7689c10b83512dba5208434c9b9d1ab1
leobyeon <386@holbertonschool.com>
2018-08-14 21:29:08 +0000

    Changed permission

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	for (strlen = 0; text_content[strlen]; strlen++)
 		;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
