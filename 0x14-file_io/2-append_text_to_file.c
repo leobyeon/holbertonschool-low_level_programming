06ec57176fdd6d1e290f01b28303d3c4b98dc326
leobyeon <386@holbertonschool.com>
2018-08-14 22:05:54 +0000

    Fixed betty check

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (!text_content)
 	{
 		close(fd);
