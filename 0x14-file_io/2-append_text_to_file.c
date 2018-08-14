49a177510f2cdd7add0b3afb3de65f6fd4b7fb71
leobyeon <386@holbertonschool.com>
2018-08-14 22:04:58 +0000

    Fixed the fail check for if text_content is null

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -20,6 +20,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 
 	for (len = 0; text_content[len]; len++)
 		;
