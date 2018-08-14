ec26243300b3f136d7b87b79c635f4f62944ba96
leobyeon <386@holbertonschool.com>
2018-08-14 21:13:27 +0000

    Switched from creat to open

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,21 +17,24 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
 	for (strlen = 0; text_content[strlen]; strlen++)
 		;
 
-	fd = creat(filename, O_RDWR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	close(fd);
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
 
 	var_write = write(fd, text_content, strlen + 1);
 	if (var_write == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }
