44945b846c8ed3e7ca6cd49e90972d95e34ad201
leobyeon <386@holbertonschool.com>
2018-08-15 01:33:58 +0000

    Deleted a fail checker

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -21,12 +21,6 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
 	/* open file_from */
 
 	openfd1 = open(argv[1], O_RDONLY);
