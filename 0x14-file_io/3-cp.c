8a749569117b404f5888d4ad2604c748cc7f62be
leobyeon <386@holbertonschool.com>
2018-08-15 03:46:28 +0000

    Restoring the old file

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int openfd1, openfd2, errormsg;
-	ssize_t var_write = 0, var_read = 1;
+	ssize_t var_write = 0, var_read = 0;
 	char buf[1024];
 
 	if (argc != 3)
@@ -41,36 +41,10 @@ int main(int argc, char *argv[])
 
 	/* read from file_from */
 
-	while (var_read) 
-	{
+	do {
 		var_read = read(openfd1, buf, 1024);
 		if (var_read == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-
-	/* write to file_to */
-
-		var_write = write(openfd2, buf, var_read);
-		if (var_write != var_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-
-	errormsg = close(openfd1);
-	if (errormsg == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", openfd1);
-		exit(100);
-	}
-	errormsg = close(openfd2);
-	if (errormsg == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", openfd2);
-		exit(100);
-	}
-	return (0);
-}
