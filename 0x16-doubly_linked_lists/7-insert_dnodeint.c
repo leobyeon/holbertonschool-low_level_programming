a1cb9dd531f9695db29cedf5e55bceee1fd5e223
leobyeon <386@holbertonschool.com>
2018-09-16 01:22:58 +0000

    Added an extra condition in the forloop

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,15 +17,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	else
 	{
-		for (idxcounter = 0; idxcounter < idx - 1; idxcounter++)
-			traverse = traverse->next;
-		if (!traverse)
-			return (NULL);
-
 		new = malloc(sizeof(dlistint_t));
 		if (!new)
 			return (NULL);
 		new->n = n;
+
+		for (idxcounter = 0; idxcounter < idx - 1 && traverse; idxcounter++)
+			traverse = traverse->next;
+		if (!traverse)
+			return (NULL);
 
 		new->next = traverse->next;
 		if (traverse->next)
