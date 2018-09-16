e07312827c12074a2fb8063f87879da7c31b91f4
leobyeon <386@holbertonschool.com>
2018-09-16 00:45:33 +0000

    Cleaned up code to be less than 40 for betty and reworked the logic in the process

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,50 +5,33 @@
  * @h: pointer to head of list
  * @idx: index of the list where the new node should be added
  * @n: value to add
- * Return: address of the new node, or
- * NULL if it failed
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int idxcounter;
-	dlistint_t *new;
-	dlistint_t *traverse;
+	dlistint_t *new, *traverse = *h;
 
-	traverse = *h;
+	if (!idx)
+		return (add_dnodeint(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	for (idxcounter = 0; idxcounter < idx-1; idxcounter++)
-	{
-		traverse = traverse->next;
-	}
-
-	if (!*h)
-	{
-		*h = new;
-	}
 	else
 	{
-		if (traverse->next && idx != 0)
-		{
-			new->next = traverse->next;
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+
+		for (idxcounter = 0; idxcounter < idx - 1; idxcounter++)
+			traverse = traverse->next;
+
+		if (!traverse)
+			return (NULL);
+		new->next = traverse->next;
+		if (traverse->next)
 			(traverse->next)->prev = new;
-			traverse->next = new;
-			new->prev = traverse;
-			traverse = new;
-		}
-		else if (!traverse->next && idx != 0)
-		{
-			traverse->next = new;
-			new->prev = traverse;
-			traverse = new;
-		}
+		new->prev = traverse;
+		traverse->next = new;
+		return (new);
 	}
-	return (new);
 }
