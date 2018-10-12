9786d5947e899898ed9cf5d7ba5e2b7b40e2fced
leobyeon <386@holbertonschool.com>
2018-10-12 20:32:29 +0000

    Initial push

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to update
+ * @key: is the key
+ * @value: is the value
+ * Return: 1 if success, 0 if failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *newnode;
+
+	if (key == NULL)
+	{
+		return (0);
+	}
+	idx = key_index((const unsigned char *)key, sizeof(ht));
+	
+	newnode = malloc(sizeof(hash_node_t));
+	if (!newnode)
+		return (0);
+
+	newnode->key = strdup(key);
+	if (!newnode->key)
+		return (0);
+	newnode->value = strdup(value);
+	if (!newnode->value)
+	{
+		free(newnode->key);
+		return (0);
+	}
+
+	newnode->next = ht->array[idx];
+	ht->array[idx] = newnode;
+
+	return (1);
+}
