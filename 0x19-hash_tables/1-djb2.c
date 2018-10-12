79b98de29c30fd3205925d72513d574053076bf1
leobyeon <386@holbertonschool.com>
2018-10-12 20:38:54 +0000

    Added comments for 1

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implements djb2 algorithm
+ * @str: string
+ * Return: hash
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
