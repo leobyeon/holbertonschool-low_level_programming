8070778b714d3a4e30122481c10796759501af1e
leobyeon <386@holbertonschool.com>
2018-11-17 01:32:20 +0000

    Adding 5

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """ returns the perimeter of the island described in grid """
+    island = 0
+    edges = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                island += 1
+            if i > 0 and grid[i-1][j] == 1:
+                edges += 1
+            if j > 0 and grid[i][j-1] == 1:
+                edges += 1
+    return island * 4 - edge * 2
