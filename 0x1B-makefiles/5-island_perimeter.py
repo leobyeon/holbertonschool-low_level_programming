b684e3a02806471a8fd1e7e77d1644d9d43fdef6
leobyeon <386@holbertonschool.com>
2018-11-17 01:37:52 +0000

    Fixed indentation errors/typo

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -10,8 +10,9 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 island += 1
-            if i > 0 and grid[i-1][j] == 1:
-                edges += 1
-            if j > 0 and grid[i][j-1] == 1:
-                edges += 1
-    return island * 4 - edge * 2
+                if i > 0 and grid[i-1][j] == 1:
+                    edges += 1
+                if j > 0 and grid[i][j-1] == 1:
+                    edges += 1
+
+    return (island * 4 - edges * 2)
