81f9f4c3b2cd38645a3767ee79eccae8853c8951
leobyeon <386@holbertonschool.com>
2018-12-12 21:27:19 +0000

    Refactored with a loop

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -7,26 +7,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_d = 0;
-	size_t right_d = 0;
+	size_t depth = 0;
 
 	if (!tree)
 		return (0);
 
-	if (tree->left)
+	while (tree->parent)
 	{
-		left_d = 1 + binary_tree_depth(tree->left);
-		if (tree->right)
-			left_d = 1 + binary_tree_depth(tree->right);
+		depth++;
+		tree = tree->parent;
 	}
-	if (tree->right)
-	{
-		right_d = 1 + binary_tree_depth(tree->right);
-		if (tree->left)
-			right_d = 1 + binary_tree_depth(tree->left);
-	}
-	if (left_d >= right_d)
-		return (left_d);
 
-	return (right_d);
+	return (depth);
 }
