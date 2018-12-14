e1e7ca169fa9190fdef55b78aecaf40c7d84ec5b
leobyeon <386@holbertonschool.com>
2018-12-14 00:36:54 +0000

    Removed unnecessary if checks

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -13,11 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	if (tree->left)
-		left_h = 1 + binary_tree_height(tree->left);
-
-	if (tree->right)
-		right_h = 1 + binary_tree_height(tree->right);
+	left_h = 1 + binary_tree_height(tree->left);
+	right_h = 1 + binary_tree_height(tree->right);
 
 	if (left_h >= right_h)
 		return (left_h);
